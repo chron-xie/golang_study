40de61d6e73fc2d8b4d21fd2acb084a34fd0da4b
Chron <chron.xie@jitstack.com>
2022-06-06 09:34:32 +0800

    first commit

--- charpter001/main/helloworld.go
+++ charpter001/main/helloworld.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "time"
+)
 
 func main(){
   fmt.Println("Hello World!")
+  var a chan string = make(chan string,2)
+  var b chan string = make(chan string,2)
+  var c chan string = make(chan string,2)
+  c <- "C"
+  time.Sleep(3*time.Second)
+  a <- "A"
+  time.Sleep(3*time.Second)
+  b <- "B"
+  for {
+    //按成绩分成ABCD等级
+    select {
+    case a <- "A":
+      fmt.Println("等级A")
+    case b <- "B":
+      fmt.Println("等级B")
+    default:
+      fmt.Println("等级C")
+    }
+  }
 }
